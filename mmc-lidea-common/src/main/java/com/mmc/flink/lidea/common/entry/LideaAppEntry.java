701d4a2945fc36198c011a975d8b566f4233f7dc
VIPJoey <yezhouwan@fcbox.com>
2019-08-29 16:23:43 +0800

    重构--增加service页

---
+++ mmc-lidea-common/src/main/java/com/mmc/flink/lidea/common/entry/LideaAppEntry.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2010-2020 Founder Ltd. All Rights Reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * Founder. You shall not disclose such Confidential Information
+ * and shall use it only in accordance with the terms of the agreements
+ * you entered into with Founder.
+ *
+ */
+package com.mmc.flink.lidea.common.entry;
+
+import com.mmc.flink.lidea.common.bo.LideaAppBO;
+import com.mmc.flink.lidea.common.context.Const;
+import com.mmc.lidea.util.BytesUtils;
+import com.mmc.lidea.util.MD5Util;
+import org.apache.hadoop.hbase.client.Put;
+
+/**
+ * 应用名称实体操作类.
+ * @author Joey
+ * @date 2019/8/29 10:48
+ */
+public class LideaAppEntry {
+
+    public static Put of(LideaAppBO bo) {
+
+
+        Put put = new Put(makeRowKey(bo));
+        put.addColumn(Const.LIDEA_LOG_FEMILY, BytesUtils.toBytes("time"), BytesUtils.toBytes(bo.time));
+        put.addColumn(Const.LIDEA_LOG_FEMILY, BytesUtils.toBytes("appName"), BytesUtils.toBytes(bo.appName));
+
+        return put;
+    }
+
+
+    private static byte[] makeRowKey(LideaAppBO bo) {
+        return BytesUtils.toBytes(MD5Util.encrypt(bo.appName));
+    }
+}
