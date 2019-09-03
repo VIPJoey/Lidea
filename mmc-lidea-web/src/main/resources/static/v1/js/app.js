c40a5c36906072195d291bce1efc12c5036081fe
VIPJoey <yezhouwan@fcbox.com>
2019-09-03 16:34:42 +0800

    首页样本填充

--- mmc-lidea-web/src/main/resources/static/v1/js/app.js
+++ mmc-lidea-web/src/main/resources/static/v1/js/app.js
@@ -10,6 +10,14 @@ function bindDataToGallery(resp) {
 
         $("#divContainer").append(html);
     }
+    // 防止控件bug抖动，加样本填充
+    for (let i = resp.data.length; i < 2; i++) {
+
+        let tpl = $("#divTpl").html();
+        let html = Nora.Util.StringUtil.format(tpl, "demo" + i, "demo" + i);
+
+        $("#divContainer").append(html);
+    }
 
     let mid = resp.data.length / 2;
 
